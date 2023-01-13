0b9e09a9a67c94af76c06acdd330c65a0c98a9d9
hoadh <danghuyhoa@gmail.com>
2023-01-13 16:50:37 +0700

    spring-overview

--- src/main/java/cg/wbd/grandemonstration/model/Customer.java
+++ src/main/java/cg/wbd/grandemonstration/model/Customer.java
@@ -1,12 +1,6 @@
 package cg.wbd.grandemonstration.model;
 
-import javax.persistence.*;
-
-@Entity
-@Table
 public class Customer implements Cloneable {
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String name;
     private String email;
